dfec89ab3c5791dbf4d8ae969941763be5409d47
James <jmb47580@ucmo.edu>
2019-03-16 09:49:04 -0500

    Created user repository interface for extending JPA's JpaRepository

---
+++ src/main/java/com/james/poll/config/AuditingConfig.java
@@ -0,0 +1,11 @@
+package com.james.poll.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class AuditingConfig {
+    //more auditing later
+}
