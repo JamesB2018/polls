0ac0d53cdfb6d8942c123d167a197184546b1c6b
James <jmb47580@ucmo.edu>
2019-03-17 12:11:01 -0500

    Set up user Details and has them use Spring Security for username or email.

---
+++ src/main/java/com/james/poll/Security/CustomUserDetailsService.java
@@ -0,0 +1,40 @@
+package com.james.poll.Security;
+
+
+import com.james.poll.model.User;
+import com.james.poll.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String usernameOrEmail)
+        throws UsernameNotFoundException {
+        //Let people log in with either a username or email
+        User user = userRepository.findByUsernameOrEmail(usernameOrEmail, usernameOrEmail)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found with username or email : " + usernameOrEmail)
+                );
+        return UserPrincipal.create(user);
+    }
+    //This method is used by JWTAuthenticationFilter
+    @Transactional
+    public UserDetails loadUserById(Long id) {
+        User user = userRepository.findById(id).orElseThrow(
+                () -> new UsernameNotFoundException("User not foudn with : " + id)
+        );
+
+        return UserPrincipal.create(user);
+    }
+}
